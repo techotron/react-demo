09a335fc8a83a47ca3aa94a3ab69ebcb576d4765
esnow <edward.snow@disneystreaming.com>
2020-11-03 00:18:33 +0000

    output ip

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const localIpUrl = require('local-ip-url');
+
 function Router(): any {
     return (
         <div>
             <BrowserRouter>
                 <Route exact
                     path='/'
-                    render={() => <span><h1>Welcome to React Demo!</h1></span>}
+                    render={() =>
+                        <span>
+                            <h1>Welcome to React Demo!</h1>
+                            <h3>IP Address: <b>{localIpUrl()}</b></h3>
+                        </span>
+                    }
                 />
             </BrowserRouter>
         </div>
